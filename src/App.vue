2300c94b580d322f68d8e60eb8c1a8c9dedcf4f6
sanjaymehra2 <85385037+sanjaymehra2@users.noreply.github.com>
2021-06-24 18:43:27 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <v-toolbar class="green white-text">
+      <router-link to="/">Home</router-link> 
+      <v-spacer></v-spacer>
+      <router-link to="/about">About</router-link>
+      </v-toolbar>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+  background-color: cornflowerblue;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
